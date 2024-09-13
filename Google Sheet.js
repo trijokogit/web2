457de9f65b7808857132642b0575206d1d86e11a
trijokogit <68747610+trijokogit@users.noreply.github.com>
2024-09-13 07:38:44 +0700

    coba hapus

--- Google Sheet.js
+++ Google Sheet.js
@@ -1,11 +1,1 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycby3eRgpDno6uRiJa8fElhAKuRFJlBz3sr6xDOdciK5aXgIUrk3kLLk-w9qNxgRUSpadqA/exec'
 
-const form = document.forms['contact-form']
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
